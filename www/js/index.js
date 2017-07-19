fdad56a5572c9c2cfca4f067ec844c1fcd7f91d5
Josh Johnson <josh@resultsonlyweb.com>
2017-07-19 15:11:34 -0400

    work

--- www/js/index.js
+++ www/js/index.js
@@ -16,10 +16,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-function loadSite() {
-  document.getElementById('appView').style.display = 'none';
-  document.getElementById('portalView').style.display = 'block';
-}
 var app = {
     // Application Constructor
     initialize: function() {
@@ -66,17 +62,30 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
+      if (this.checkConnection) {
+        document.getElementById('appView').style.display = 'none';
+        document.getElementById('portalView').style.display = 'block';
+      } else {
+        var ref = window.open('offline.html', '_self', 'location=no');
+      }
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    checkConnection: function() {
+        var networkState = navigator.connection.type;
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.CELL]     = 'Cell generic connection';
+        states[Connection.NONE]     = 'No network connection';
 
-        console.log('Received Event: ' + id);
+        if (states[networkState] == 'No network connection') {
+          return false;
+        } else {
+          return true;
+        }
     }
 };
