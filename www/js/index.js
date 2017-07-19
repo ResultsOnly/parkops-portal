fc6c071f84e7838d1cf24214c1085544e76e4182
Josh Johnson <josh@resultsonlyweb.com>
2017-07-19 15:17:34 -0400

    Refactor

--- www/js/index.js
+++ www/js/index.js
@@ -20,35 +20,6 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
-        document.addEventListener("deviceready", onDeviceReady, false);
-        function onDeviceReady() {
-            console.log('Application Ready');
-            if (checkConnection()) {
-              document.getElementById('appView').style.display = 'none';
-              document.getElementById('portalView').style.display = 'block';
-            } else {
-              var ref = window.open('offline.html', '_self', 'location=no');
-            }
-        }
-        function checkConnection() {
-            var networkState = navigator.connection.type;
-
-            var states = {};
-            states[Connection.UNKNOWN]  = 'Unknown connection';
-            states[Connection.ETHERNET] = 'Ethernet connection';
-            states[Connection.WIFI]     = 'WiFi connection';
-            states[Connection.CELL_2G]  = 'Cell 2G connection';
-            states[Connection.CELL_3G]  = 'Cell 3G connection';
-            states[Connection.CELL_4G]  = 'Cell 4G connection';
-            states[Connection.CELL]     = 'Cell generic connection';
-            states[Connection.NONE]     = 'No network connection';
-
-            if (states[networkState] == 'No network connection') {
-              return false;
-            } else {
-              return true;
-            }
-        }
     },
     // Bind Event Listeners
     //
@@ -62,30 +33,11 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-      if (this.checkConnection) {
+      if (navigator.onLine) {
         document.getElementById('appView').style.display = 'none';
         document.getElementById('portalView').style.display = 'block';
       } else {
         var ref = window.open('offline.html', '_self', 'location=no');
       }
-    },
-    checkConnection: function() {
-        var networkState = navigator.connection.type;
-
-        var states = {};
-        states[Connection.UNKNOWN]  = 'Unknown connection';
-        states[Connection.ETHERNET] = 'Ethernet connection';
-        states[Connection.WIFI]     = 'WiFi connection';
-        states[Connection.CELL_2G]  = 'Cell 2G connection';
-        states[Connection.CELL_3G]  = 'Cell 3G connection';
-        states[Connection.CELL_4G]  = 'Cell 4G connection';
-        states[Connection.CELL]     = 'Cell generic connection';
-        states[Connection.NONE]     = 'No network connection';
-
-        if (states[networkState] == 'No network connection') {
-          return false;
-        } else {
-          return true;
-        }
     }
 };
